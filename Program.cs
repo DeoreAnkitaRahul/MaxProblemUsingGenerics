58957100584e07ba4d7f63eccaa82ea82d4b52f0
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-02-22 14:13:08 +0530

    Find Max Integer

--- Program.cs
+++ Program.cs
@@ -5,7 +5,12 @@
         public static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome To Max Problem Using Generics");
+           // Console.WriteLine("Welcome To Max Problem Using Generics");
+            int num1 = 70, num2 = 120, num3 = 100;
+
+            int value = MaxNo.MaximumIntegerNumber(num1, num2, num3);
+
+            Console.WriteLine(value + " is greatest");
         }
     }
 }
